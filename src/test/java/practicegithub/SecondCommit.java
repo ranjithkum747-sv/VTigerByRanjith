1bb07c2f2d1ef3c8b6c82ee04f3db4df12f3a6d1
Anbuit002 <Anbuit002@www.easeus.com>
2025-12-30 15:56:29 +0530

    newly adding

---
+++ src/test/java/practicegithub/SecondCommit.java
@@ -0,0 +1,19 @@
+package practicegithub;
+
+import org.testng.annotations.Test;
+
+public class SecondCommit {
+	
+	@Test
+	
+	 public void m1()
+	 {
+		
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+	 }
+
+}
