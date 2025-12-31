06b4335be0b591f5fd41e9e98648c3f2ba51a8a5
Anbuit002 <Anbuit002@DESKTOP-7EFKBAF>
2025-12-31 12:12:56 +0530

    this is changed

--- src/test/java/practicegithub/FirstCommit.java
+++ src/test/java/practicegithub/FirstCommit.java
@@ -1,8 +1,11 @@
 package practicegithub;
 
+import org.testng.annotations.Test;
+
 public class FirstCommit 
 {
-	public static void main(String[] args) 
+	@Test
+	public void firstCommitMethod()
 	{
 		System.out.println("Hello");
 		
